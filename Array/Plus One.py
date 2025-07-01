37d201ec7da38ae8eef44e8d5efa2dc3cbaecd11
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2025-06-30 21:13:28 -0400

    Update Plus One.py

--- Array/Plus One.py
+++ Array/Plus One.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        b = ""
+        for i in digits:
+            b += str(i)
+        digits = list(str(int(b) + 1))
+        out = []
+        for i in digits:
+            out.append(int(i))
+        return out
